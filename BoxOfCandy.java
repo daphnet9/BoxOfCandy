86e75d130185d5df6c8893cdaabae8d3c7799dfd
daphnet9 <daphnet9@nycstudents.net>
2025-01-08 16:39:51 +0000

    candy box setup

---
+++ BoxOfCandy.java
@@ -0,0 +1,19 @@
+public class BoxOfCandy {
+    private Candy[][] box;
+    // public boolean moveCandyToFirstRow(int col)
+    // public Candy removeNextByFlavor(String flavor)
+
+    public BoxOfCandy(Candy[][] b) {
+        box = b;
+    }
+    public String toString() {
+        String s = "";
+        for (int row = 0; row < box.length; row++) {
+            for (int col = 0; col < box[0].length; col++) {
+                s += box[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+}
---
+++ Candy.java
@@ -0,0 +1,12 @@
+public class Candy {
+    // public String getFlavor() {
+        
+    // }
+    private String flavor;
+    public Candy(String f) {
+        flavor = f;
+    }
+    public String toString() {
+        return flavor;
+    }
+}
